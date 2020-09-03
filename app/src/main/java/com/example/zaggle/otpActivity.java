acc1278ea19fe4c76608903c121037d3b2f5145d
Nitin Gupta <nitind4777@gmail.com>
2020-09-03 14:44:37 +0530

    Second Commit

--- app/src/main/java/com/example/zaggle/otpActivity.java
+++ app/src/main/java/com/example/zaggle/otpActivity.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class otpActivity extends AppCompatActivity {
 
-    public void forgot(View view){
-        Intent intent = new Intent(getApplicationContext(),otpActivity.class);
+    public void send(View view){
+        Intent intent = new Intent(getApplicationContext(),ChangeActivity.class);
         startActivity(intent);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_otp);
     }
 }
